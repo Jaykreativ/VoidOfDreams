84d2041f4918531c9720e38e6d768b61219e7f7e
Jaykreativ <zauggjunk4@outlook.com>
2025-03-10 17:24:00 +0100

    basic log functions (WIP)

--- src/Log.cpp
+++ src/Log.cpp
@@ -21,4 +21,32 @@ namespace log {
 		_regionStack.pop();
 	}
 
+	void error(std::string msg) {
+		fprintf(stderr, "%s", msg.c_str());
+	}
+	void errorRaw(std::string msg) {
+		fprintf(stderr, "%s", msg.c_str());
+	}
+
+	void warning(std::string msg) {
+		printf("%s", msg.c_str());
+	}
+	void warningRaw(std::string msg) {
+		printf("%s", msg.c_str());
+	}
+
+	void info(std::string msg) {
+		printf("%s", msg.c_str());
+	}
+	void infoRaw(std::string msg) {
+		printf("%s", msg.c_str());
+	}
+
+	std::string getCurrentRegion() {
+
+	}
+
+	const std::stack<std::string, std::vector<std::string>>& getRegionStack() {
+
+	}
 }
--- src/Log.h
+++ src/Log.h
@@ -9,6 +9,15 @@ namespace log {
 
 	void endRegion();
 
+	void error(std::string msg);
+	void errorRaw(std::string msg);
+
+	void warning(std::string msg);
+	void warningRaw(std::string msg);
+
+	void info(std::string msg);
+	void infoRaw(std::string msg);
+
 	std::string getCurrentRegion();
 
 	const std::stack<std::string, std::vector<std::string>>& getRegionStack();
