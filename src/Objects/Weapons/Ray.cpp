5cb3491fd7f9a115a6a25158549e6a7456978e16
Jaykreativ <janzaugg@bluewin.ch>
2025-05-21 17:59:38 +0200

    ray filter

--- src/Objects/Weapons/Ray.cpp
+++ src/Objects/Weapons/Ray.cpp
@@ -1,12 +1,25 @@
 #include "Ray.h"
 
+#include "Zap/Scene/Actor.h"
+
 Ray::Ray(WorldData& world)
 	: m_world(world)
 {}
 
-class RayFilter : physx::PxQueryFilterCallback {
-	physx::PxQueryHitType::Enum preFilter(const physx::PxFilterData& filterData, const physx::PxShape* shape, const physx::PxRigidActor* actor, physx::PxHitFlags& queryFlags) {
+class RayFilter : public physx::PxQueryFilterCallback {
+public:
+	Zap::Actor excludedActor;
 
+	physx::PxQueryHitType::Enum preFilter(const physx::PxFilterData& filterData, const physx::PxShape* shape, const physx::PxRigidActor* actor, physx::PxHitFlags& queryFlags) {
+		PX_UNUSED(filterData); PX_UNUSED(shape); PX_UNUSED(shape); PX_UNUSED(queryFlags);
+		if ((uint64_t)(actor->userData) == excludedActor.getHandle())
+			return physx::PxQueryHitType::eNONE;
+		return physx::PxQueryHitType::eBLOCK;
+	}
+
+	physx::PxQueryHitType::Enum postFilter(const physx::PxFilterData& filterData, const physx::PxQueryHit& hit, const physx::PxShape* shape, const physx::PxRigidActor* actor) {
+		PX_UNUSED(filterData); PX_UNUSED(hit); PX_UNUSED(shape); PX_UNUSED(actor);
+		return physx::PxQueryHitType::eBLOCK;
 	}
 };
 
@@ -14,7 +27,9 @@ void Ray::update(Player& player, PlayerInventory::iterator iterator) {
 	if (m_isTriggered) {
 		m_isTriggered = false; // one time trigger
 		Zap::Scene::RaycastOutput out = {};
-		if (m_world.scene->raycast(player.getTransform()[3], player.getCameraTransform()[2], 1000, &out)) {
+		RayFilter filter;
+		filter.excludedActor = player.getPhysicsActor();
+		if (m_world.scene->raycast(player.getTransform()[3], player.getCameraTransform()[2], 1000, &out, &filter)) {
 			printf("ray hit (%fm)\n", out.distance);
 		}
 	}
