89bd3a630e0aec49adc3a846dd9b5072f71df2d3
Jaykreativ <zauggjunk4@outlook.com>
2025-05-23 08:55:26 +0200

    add Frameviewer to logger, record frame profile

--- src/Log.h
+++ src/Log.h
@@ -3,12 +3,12 @@
 #include <string>
 #include <vector>
 
+// for time profiles use
+// #define LOG_GLOBAL_TIMESTAMPS for a global timeline
+// #define LOG_FRAME_TIMESTAMPS for a frame profile
+#define LOG_GLOBAL_TIMESTAMPS
+#define LOG_FRAME_TIMESTAMPS
 namespace logger {
-	void initLog();
-
-	void terminateLog();
-
-
 	void beginRegion(std::string name);
 
 	void endRegion();
@@ -22,17 +22,22 @@ namespace logger {
 	void info(std::string msg);
 	void infoRaw(std::string msg);
 
-
 	std::string getCurrentRegion();
 
 	const std::vector<std::string>& getRegionStack();
 
-	// delete old profiles
-	// this has to be called from time to time to not run out of memory
 	// duration in seconds
 	void setTimelineValidDuration(float duration);
 
+	// delete old profiles
+	// this has to be called from time to time to not run out of memory
 	void cleanTimeline();
 
+	void beginFrame();
+
+	void endFrame();
+
 	void drawTimelineImGui();
+
+	void drawFrameProfileImGui();
 }
