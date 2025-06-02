cd5b982b8be999d9a8a23d304f5364807e8711f9
Jaykreativ <janzaugg@bluewin.ch>
2025-06-02 21:19:06 +0200

    set domain name as default ip

--- src/Shares/NetworkData.h
+++ src/Shares/NetworkData.h
@@ -5,7 +5,7 @@
 struct NetworkData {
 	std::string username = "user"; // this username serves as an id for the client
 	std::string port = "12525";
-	std::string ip = "127.0.0.1";
+	std::string ip = "zap.internet-box.ch";
 
 	// server specific
 	int backlog = 10;
